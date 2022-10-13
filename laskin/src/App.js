33f3bec70c32e7cd1807d64e7bb2266198c0e01d
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-10-13 09:49:35 +0300

    additional buttons and functions and error catcher

--- laskin/src/App.js
+++ laskin/src/App.js
@@ -2,13 +2,45 @@ import "./App.css";
 import Nappain from "./Nappain";
 import { useState } from "react";
 
-let nappaimet = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "="];
+let nappaimet = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "+",
+  "-",
+  "/",
+  "*",
+  "=",
+  "AC",
+  "(",
+  ")",
+];
 
 function App() {
   const [teksti, setTeksti] = useState("");
   const nappainPainettu = (x) => {
     if (x == "=") {
       setTeksti(eval(teksti));
+      return;
+    }
+    if (x == "AC") {
+      setTeksti("");
+      return;
+    }
+    if (
+      (teksti.slice(-1) == "+" ||
+        teksti.slice(-1) == "-" ||
+        teksti.slice(-1) == "*" ||
+        teksti.slice(-1) == "/") &&
+      (x == "+" || x == "-" || x == "*" || x == "/")
+    ) {
+      setTeksti("ERROR");
       return;
     }
     setTeksti(teksti + x);
@@ -23,6 +55,7 @@ function App() {
           nappainPainettu={nappainPainettu}
           nappain={nappain}
         />
+        // key, nappainPainettu and nappain are props that are passed to the child component Nappain
       ))}
     </div>
   );
