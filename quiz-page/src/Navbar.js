ab830d27d3384fdb39f4417e87830e37482c0270
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-10-18 11:32:43 +0300

    further tweaks on Navbar

--- quiz-page/src/Navbar.js
+++ quiz-page/src/Navbar.js
@@ -4,10 +4,6 @@ import "./Answers";
 import "./Quizzes";
 
 const Navbar = (props) => {
-  const quiz = props.quizzes.map((quiz) => {
-    return <a href="#">{quiz}</a>;
-  });
-
   return (
     <div>
       <div className="Navigation-bar">
@@ -15,7 +11,7 @@ const Navbar = (props) => {
           <button className="dropbtn">Quizzes</button>
           <div className="dropdown-content">
             {props.quizzes.map((quiz) => {
-              return <a href="#">{quiz}</a>;
+              return <a href="#">{quiz.quizName}</a>;
             })}
           </div>
         </div>
@@ -29,26 +25,3 @@ const Navbar = (props) => {
 };
 
 export default Navbar;
-
-// const Navbar = (props) => {
-//     return (
-//       <div>
-//         <div className="Navigation-bar">
-//           <div className="dropdown">
-//             <button className="dropbtn">Quizzes</button>
-//             <div className="dropdown-content">
-//               <a href="#">Quiz 1</a>
-//               <a href="#">Quiz 2</a>
-//               <a href="#">Quiz 3</a>
-//             </div>
-//           </div>
-//           <a href="Help.asp">Help</a>
-//           <div className="Float-right">
-//             <a href="Quit.asp">Quit</a>
-//           </div>
-//         </div>
-//       </div>
-//     );
-//   };
-
-//   export default Navbar;
