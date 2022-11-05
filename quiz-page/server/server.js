8416284ed2444378dfd43e3ab838b5a6c1844358
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-11-05 09:22:48 +0200

    edits on server

--- quiz-page/server/server.js
+++ quiz-page/server/server.js
@@ -5,6 +5,15 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
+const { Pool, Client } = require("pg");
+const pool = new Pool({
+  user: "postgres",
+  host: "localhost",
+  database: "postgres",
+  password: "admin",
+  port: 5432,
+});
+
 app.use(cors()); //jos ei toimi, niin "npm install cors"
 app.use(express.json());
 
@@ -13,6 +22,47 @@ app.use(bodyparser.json());
 
 // let data = fs.readFileSync("./kouludata.json", { encoding: "utf8", flag: "r" });
 let data;
+
+// add question to question table
+// function
+// {quizID: 2, question: "What is?"}
+// instead of questionObject we could also add quizID and question
+const addQuestion = (questionObject) => {
+  values = [questionObject.quizId, questionObject.question];
+  try {
+    dbpool.query(
+      "INSERT INTO question (exam_id, question) VALUES ($1, $2) ",
+      values
+    );
+  } catch (e) {}
+};
+
+// add question to question table
+// as database request
+// code has been tested with POSTMAN and makes the addition to db
+app.post("/questions", async (req, res) => {
+  //const id = Number(req.params.id); // code does not seem to be needed
+  console.log("Now we add a question");
+  try {
+    await pool.query(
+      "INSERT INTO questions (quiz_id, name) VALUES ($1, $2) ",
+      [req.body.quizId, req.body.name] // req.body is what comes from POSTMAN
+    );
+    res.send("saving data succeeded!"); // message sent to POSTMAN
+  } catch (e) {
+    res.status(500).send("Error!");
+    console.log(e);
+  }
+});
+
+app.post("/questions", (req, res) => {
+  res.send("Got a POST request in questions");
+});
+
+app.get("/questions", (req, res) => {
+  res.send("Hello Questions!");
+});
+
 app.get("/", (req, res) => {
   //SELECT
   console.log("Data reqiested from server");
