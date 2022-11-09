c9c72bf088a701a3ca7f2b5e4db66e1537eae1a3
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-11-09 11:28:38 +0200

    code along REST API tutorial

---
+++ RESTAPI/src/student/routes.js
@@ -0,0 +1,16 @@
+const { Router } = require("express");
+const controller = require("./controller");
+
+const router = Router();
+
+router.get("/", controller.getStudents);
+router.post("/", controller.addStudent);
+router.get("/:id", controller.getStudentsById);
+router.put("/:id", controller.updateStudent);
+router.delete("/:id", controller.removeStudent);
+
+router.get("/", (req, res) => {
+  res.send("using api route");
+});
+
+module.exports = router;
