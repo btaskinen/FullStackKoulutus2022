c4d0ae0e412d2a11c21e86178ed8a7a0758c3cfd
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-11-18 14:31:58 +0200

    email function added

--- quiz-page/server/server.js
+++ quiz-page/server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const quizRoutes = require("../src/quizdb/routes");
 const https = require("https");
 const fs = require("fs");
+const nodemailer = require("nodemailer"); // to send email from the server
 
 const app = express();
 const port = 8080;
@@ -42,3 +43,27 @@ https
 app.get("/", (req, res) => {
   res.send("Hello from https express server!");
 });
+
+// -------------------------------SENDING EMAIL FROM THE SERVER ---------------------------
+// const transporter = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASSWORD,
+//   },
+// });
+
+// const mailOptions = {
+//   from: "barbara.taskinen@gmail.com",
+//   to: "barbara.taskinen@gmail.com, juvuorin@gmail.com",
+//   subject: "Terveisiä Barbaran Nodejs-serverilta",
+//   text: "Moi! Jos saat tämän viestin, onnistuin pikkutehtävässä :-)",
+// };
+
+// transporter.sendMail(mailOptions, (error, info) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Email sent:" + info.response);
+//   }
+// });
