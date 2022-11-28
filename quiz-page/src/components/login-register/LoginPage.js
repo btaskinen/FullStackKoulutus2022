a6deecd3de51f0d6b27d71829ebc3cd6a4be26da
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-11-28 10:09:28 +0200

    updates on login and register

--- quiz-page/src/components/login-register/LoginPage.js
+++ quiz-page/src/components/login-register/LoginPage.js
@@ -1,35 +1,44 @@
 import "./LoginPage.css";
 import { useRef } from "react";
-import axios from "axios";
 
 const LoginPage = (props) => {
   const emailInputRef = useRef(null);
   const passwordInputRef = useRef(null);
 
-  const loginUser = async (email, password) => {
-    try {
-      const result = await axios.post(
-        `https://localhost:8080/api/quiz-page//users/login`,
-        {
-          user_email: email,
-          password: password,
-        }
-      );
-      const token = result.data.data.token;
-      if (token) {
-        props.loginHandler();
-        localStorage.setItem("loginToken", token);
-      }
-    } catch (result) {
-      alert(result.response.data);
-    }
-  };
+  // const loginUser = async (email, password) => {
+  //   try {
+  //     const result = await axios.post(
+  //       `https://localhost:8080/api/quiz-page//users/login`,
+  //       {
+  //         user_email: email,
+  //         password: password,
+  //       }
+  //     );
+  //     const token = result.data.data.token;
+  //     if (token) {
+  //       props.loginHandler();
+  //       localStorage.setItem("loginToken", token);
+  //     }
+  //   } catch (result) {
+  //     alert(result.response.data);
+  //   }
+  // };
 
   const submitHandler = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
 
-    loginUser(enteredEmail, enteredPassword);
+    props.loginUser(enteredEmail, enteredPassword);
+  };
+
+  const visibilityHandler = () => {
+    let textField = document.getElementById("password");
+
+    if (textField.type === "password") {
+      textField.type = "text";
+    } else {
+      textField.type = "password";
+    }
   };
 
   return (
@@ -38,23 +47,28 @@ const LoginPage = (props) => {
         <h1>Welcome to the Quiz Page!</h1>
         <p>Please Login or create new user account by registering</p>
         <div className="login-container">
-          <div>
-            <label className="label">User Name</label>
+          <label className="label">User Email</label>
+          <input
+            className="login-text-field"
+            type="text"
+            placeholder="User Name"
+            ref={emailInputRef}
+          />
+          <label className="label">Password</label>
+          <input
+            className="login-text-field"
+            type="password"
+            placeholder="Password"
+            ref={passwordInputRef}
+            id="password"
+          />
+          <div className="show-password">
             <input
-              className="login-text-field"
-              type="text"
-              placeholder="User Name"
-              ref={emailInputRef}
+              type="checkbox"
+              className="checkbox"
+              onClick={visibilityHandler}
             />
-          </div>
-          <div>
-            <label className="label">Password</label>
-            <input
-              className="login-text-field"
-              type="text"
-              placeholder="Password"
-              ref={passwordInputRef}
-            />
+            <label className="checkbox">Show Password</label>
           </div>
           <div className="button-container">
             <button className="login-button" onClick={submitHandler}>
--- quiz-page/src/components/login-register/StartPage.js
+++ quiz-page/src/components/login-register/StartPage.js
@@ -3,9 +3,29 @@ import Navbar from "../Navbar";
 import { useState } from "react";
 import LoginPage from "./LoginPage";
 import RegisterPage from "./RegisterPage";
+import axios from "axios";
 
 const StartPage = (props) => {
   const [register, setRegister] = useState(false);
+
+  const loginUser = async (email, password) => {
+    try {
+      const result = await axios.post(
+        `https://localhost:8080/api/quiz-page//users/login`,
+        {
+          user_email: email,
+          password: password,
+        }
+      );
+      const token = result.data.data.token;
+      if (token) {
+        props.loginHandler();
+        localStorage.setItem("loginToken", token);
+      }
+    } catch (result) {
+      alert(result.response.data);
+    }
+  };
 
   const registerHandler = () => {
     setRegister((current) => !current);
@@ -23,12 +43,14 @@ const StartPage = (props) => {
         <LoginPage
           registerHandler={registerHandler}
           loginHandler={props.loginHandler}
+          loginUser={loginUser}
         />
       )}
       {register && (
         <RegisterPage
           registerHandler={registerHandler}
           loginHandler={props.loginHandler}
+          loginUser={loginUser}
         />
       )}
     </div>
