ad1460b28825197822275da77c24151eaaedac34
Barbara Taskinen <barbara.taskinen@gmail.com>
2022-12-01 15:59:03 +0200

    checkboxes added

--- quiz-page/src/components/user-components/UserAnswers.js
+++ quiz-page/src/components/user-components/UserAnswers.js
@@ -1,7 +1,18 @@
 import "./UserAnswers.css";
 
 const UserAnswers = (props) => {
-  return <div className="style-answers">{props.answer.answer_text}</div>;
+  return (
+    <div className="style-answers">
+      <input
+        className="checkbox"
+        type="checkbox"
+        id={props.answer.answer_text}
+        name={`response to question ${props.question.question_id}`}
+        value={props.answer.correct_answer}
+      />
+      <label htmlFor={props.answer.answer_id}>{props.answer.answer_text}</label>
+    </div>
+  );
 };
 
 export default UserAnswers;
