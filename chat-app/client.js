26f45aae732226087288950efe4173dc466acf26
Barbara Taskinen <barbara.taskinen@gmail.com>
2023-01-10 16:29:41 +0200

    added functionalities to WebSocket chat-app

--- chat-app/client.js
+++ chat-app/client.js
@@ -4,33 +4,139 @@ let username;
 
 socket.onopen = () => {
   username = prompt("Welcome to the chat! Please provide username: ");
-  let message = `Welcome to the chat! You are connected as ${username}.`;
-  const node = document.createElement("P");
-  const text = document.createTextNode(message);
-  node.style.cssText = "color:#BB230B;font-weight:bold";
-  console.log(message);
-  node.appendChild(text);
-  document.getElementById("chatbox").appendChild(node);
+  const messageList = [
+    `You have been connected to the chat server as ${username}.`,
+    "Note that swearing will lead to immediate removal from chat.",
+    'For Instructions type "!help"',
+  ];
+  messageList.forEach((el) => {
+    const node = document.createElement("P");
+    const text = document.createTextNode(el);
+    node.style.cssText = "color:#087f5b;font-weight:bold";
+    console.log(message);
+    node.appendChild(text);
+    document.getElementById("chatbox").appendChild(node);
+  });
+
+  socket.send(
+    JSON.stringify({
+      type: "CONNECT",
+      username: username,
+    })
+  );
 };
 
 socket.onmessage = ({ data }) => {
-  const node = document.createElement("P");
-  const text = document.createTextNode(data);
-  console.log(data);
-  node.style.cssText = "font-weight:bold";
-  node.appendChild(text);
-  document.getElementById("chatbox").appendChild(node);
+  const dataObject = JSON.parse(data);
+  let node;
+  let text;
+
+  const test = typeof dataObject.message === "object";
+
+  if (test) {
+    dataObject.message.forEach((el) => {
+      const node = document.createElement("P");
+      text = document.createTextNode(el);
+      node.style.cssText = `color:${dataObject.color};font-weight:bold`;
+      node.appendChild(text);
+      document.getElementById("chatbox").appendChild(node);
+    });
+  } else {
+    node = document.createElement("P");
+
+    if (dataObject.username) {
+      text = document.createTextNode(
+        `${dataObject.username}: ${dataObject.message}`
+      );
+    } else {
+      text = document.createTextNode(dataObject.message);
+    }
+
+    console.log(dataObject.message);
+    node.style.cssText = `color:${dataObject.color};font-weight:bold`;
+    node.appendChild(text);
+    document.getElementById("chatbox").appendChild(node);
+  }
 };
 
 const disconnect = () => {
+  socket.send(
+    JSON.stringify({
+      type: "QUIT",
+      username: username,
+      message: `${username} has left the chat.`,
+    })
+  );
   socket.close();
 };
 
 const sendmessage = () => {
-  message = document.getElementById("message").value;
-  socket.send(JSON.stringify({ username: username, message: message }));
+  data = document.getElementById("message").value;
+
+  if (data === "!quit") {
+    socket.send(
+      JSON.stringify({
+        type: "QUIT",
+        username: username,
+        message: `${username} has left the chat.`,
+      })
+    );
+    const node = document.createElement("P");
+    const text = document.createTextNode("You left the chat. Bye!");
+    node.style.cssText = "color:#087f5b;font-weight:bold";
+    node.appendChild(text);
+    document.getElementById("chatbox").appendChild(node);
+    socket.close();
+  } else if (data === "!help") {
+    socket.send(
+      JSON.stringify({
+        type: "HELP",
+      })
+    );
+  } else if (data === "!users") {
+    socket.send(
+      JSON.stringify({
+        type: "USERS",
+      })
+    );
+  } else if (data.includes("!remove")) {
+    try {
+      const dataArray = data.split(":");
+      selectedName = dataArray[1].slice(1);
+      socket.send(
+        JSON.stringify({
+          type: "REMOVE",
+          username: username,
+          selectedName: selectedName,
+        })
+      );
+    } catch (error) {
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `Wrong input. Make sure you give command correctly (!remove: username)`
+      );
+    }
+  } else if (/\$\w*\:/.test(data)) {
+    const dataArray = data.split(":", 1);
+    selectedName = dataArray[0].slice(1);
+    trimmedMessage = data.replace(dataArray[0] + ":", "");
+    // console.log("MESSAGE", trimmedMessage);
+    socket.send(
+      JSON.stringify({
+        type: "PRIVATEMESSAGE",
+        message: `PRIVATE ${username} : ${trimmedMessage}`,
+        username: username,
+        selectedName: selectedName,
+      })
+    );
+  } else {
+    socket.send(
+      JSON.stringify({ type: "MESSAGE", username: username, message: data })
+    );
+  }
+
   const node = document.createElement("P");
-  const text = document.createTextNode(message);
+  const text = document.createTextNode(data);
   console.log(message);
   node.style.cssText = "font-weight:bold";
   node.appendChild(text);
@@ -41,8 +147,7 @@ const sendmessage = () => {
 socket.onclose = (event) => {
   let message;
   if (event.wasClean) {
-    message = `[close] Connection closed cleanly,
-  code=${event.code} reason=${event.reason}`;
+    message = `You left the Chat. Connection closed.`;
   } else {
     // e.g. server process killed or network down
     // event.code is usually 1006 in this case
@@ -52,6 +157,7 @@ socket.onclose = (event) => {
   const text = document.createTextNode(message);
   console.log(message);
   node.appendChild(text);
+  node.style.cssText = "color:#087f5b;font-weight:bold";
   document.getElementById("chatbox").appendChild(node);
 };
 
@@ -61,5 +167,6 @@ socket.onerror = (error) => {
   const text = document.createTextNode(message);
   console.log(message);
   node.appendChild(text);
+  node.style.cssText = "color:#087f5b;font-weight:bold";
   document.getElementById("chatbox").appendChild(node);
 };
