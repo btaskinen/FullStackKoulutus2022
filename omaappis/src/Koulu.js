5259b813a83faca37ef6bac55bc2e424735d8abb
Barbara <barbara.taskinen@gmail.com>
2023-07-24 13:03:44 +0300

    remove unrelated exercise code

--- omaappis/src/Koulu.js
+++
@@ -1,8 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-const Koulu = () => {
-  return <div>"Moro!"</div>;
-};
-
-export default Koulu;
--- omaappis/src/Oppilas.js
+++
@@ -1,9 +0,0 @@
-import logo from "./logo.svg";
-import "./App.css";
-
-const Oppilas = (props) => {
-  return <div>{props.oppilas.nimi}</div>;
-};
-
-export default Oppilas;
-//
